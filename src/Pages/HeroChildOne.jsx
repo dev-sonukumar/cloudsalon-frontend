258d8fb81b7427cffa268b83112a31376526cd1a
sonu kumar <sonu638056@gmail.com>
2025-02-22 23:21:43 +0530

    home

--- src/Pages/HeroChildOne.jsx
+++ src/Pages/HeroChildOne.jsx
@@ -19,24 +19,8 @@ const images = [
   heroImage6,
 ];
 
-const SkeletonLoader = () => (
-  <div className="animate-pulse bg-gray-300 rounded-xl w-full h-full"></div>
-);
-
 const HeroChildOne = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const imagePromises = images.map((img) => {
-      return new Promise((resolve) => {
-        const image = new Image();
-        image.src = img;
-        image.onload = resolve;
-      });
-    });
-    Promise.all(imagePromises).then(() => setIsLoading(false));
-  }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -48,21 +32,17 @@ const HeroChildOne = () => {
   return (
     <section className="md:hidden rounded-xl">
       <div className="overflow-hidden relative w-full h-[35vh] rounded-xl">
-        {isLoading ? (
-          <SkeletonLoader />
-        ) : (
-          <motion.img
-            key={currentIndex}
-            src={images[currentIndex]}
-            alt="Slider Image"
-            className="w-full object-cover rounded-xl"
-            initial={{ opacity: 0, x: 100 }}
-            animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: -100 }}
-            transition={{ duration: 0.5 }}
-            whileHover={{ scale: 1.05 }}
-          />
-        )}
+        <motion.img
+          key={currentIndex}
+          src={images[currentIndex]}
+          alt="Slider Image"
+          className="w-full object-cover rounded-xl"
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: -100 }}
+          transition={{ duration: 0.5 }}
+          whileHover={{ scale: 1.05 }}
+        />
       </div>
     </section>
   );
